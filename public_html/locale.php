1c581632e5ad201ac301de3dd0a4ddf1cd17851a
Juga Paazmaya <olavic@gmail.com>
2011-09-14 16:04:57 +0300

    Sample commands in the comments

--- public_html/locale.php
+++ public_html/locale.php
@@ -39,8 +39,19 @@ mv renshuuSuruToki.po renshuuSuruToki.pot
 Locale should appear from the po file...
 msgfmt --strict --check --verbose -o renshuuSuruToki.mo renshuuSuruToki.po
 
-
 "C:\Program Files\Poedit\bin\msgfmt.exe" --strict --check --verbose -o renshuuSuruToki.mo renshuuSuruToki.po
+
+
+The msgmerge program merges two Uniforum style .po files together. 
+The def.po file is an existing PO file with translations which will be taken over to the newly 
+created file as long as they still match; comments will be preserved, but extracted comments and 
+file positions will be discarded. The ref.pot file is the last created PO file with up-to-date 
+source references but old translations, or a PO Template file (generally created by xgettext); 
+any translations or comments in the file will be discarded, however dot comments and 
+file positions will be preserved. 
+Where an exact match cannot be found, fuzzy matching is used to produce better results. 
+
+msgmerge -U renshuuSuruToki.po renshuuSuruToki.pot
 
 "C:\Program Files\Poedit\bin\msgmerge.exe" -U locale\fi\LC_MESSAGES\renshuuSuruToki.po locale\renshuuSuruToki.po
 
@@ -68,16 +79,6 @@ http://www.php.net/manual/en/function.sprintf.php
 * X - the argument is treated as an integer and presented as a hexadecimal number (with uppercase letters).
 
 
-The msgmerge program merges two Uniforum style .po files together. 
-The def.po file is an existing PO file with translations which will be taken over to the newly 
-created file as long as they still match; comments will be preserved, but extracted comments and 
-file positions will be discarded. The ref.pot file is the last created PO file with up-to-date 
-source references but old translations, or a PO Template file (generally created by xgettext); 
-any translations or comments in the file will be discarded, however dot comments and 
-file positions will be preserved. 
-Where an exact match cannot be found, fuzzy matching is used to produce better results. 
-
-msgmerge -U renshuuSuruToki.po renshuuSuruToki.pot
 */
 
 /**
