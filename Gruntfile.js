144c95f39c7d510accfaef5a95a2c88e67b07226
Juga Paazmaya <olavic@gmail.com>
2013-12-21 08:22:47 +0200

    Bower #5, eslint, qunit and others #8

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    copy: {
+      bower: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            cwd: 'bower_components/',
+            src: [
+              'requirejs/require.js',
+              'underscore/underscore.js',
+              'jquery/jquery.js',
+              'backbone/backbone.js'
+            ],
+            dest: 'src/js/lib/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd: 'bower_components/leaflet/src/',
+            src: [
+              '**/*.js'
+            ],
+            dest: 'src/js/lib/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            flatten: true,
+            src: 'bower_components/qunit/qunit/*.*',
+            dest: 'test/qunit',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
+
+    eslint: {
+      options: {
+      },
+      main: {
+        src: ['src/js/main.js']
+      }
+    },
+    
+    connect: {
+      qunit: {
+        options: {
+          port: 9991,
+          base: '.',
+          //keepalive: true
+        }
+      }
+    },
+  
+    qunit: {
+      all: {
+        options: {
+          urls: [
+            'http://localhost:9991/test/index_spec.html'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      main: {
+        files: 'src/js/main.js',
+        tasks: ['eslint', 'test']
+      }
+    },
+
+    requirejs: {
+      compile: {
+        options: {
+          name: 'main',
+          baseUrl: 'src/js/',
+          mainConfigFile: 'src/js/main.js',
+          out: 'src/js/optimized.js',
+          optimize: 'uglify2', // 116 KB
+          //optimize: 'none' // 341 KB
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('default', ['test']);
+  grunt.registerTask('test', ['eslint', 'connect', 'qunit']);
+
+};
