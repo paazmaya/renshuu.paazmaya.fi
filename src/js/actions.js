f4c0f674e40670e293479a7bf94e23583e2daf45
Juga Paazmaya <paazmaya@yahoo.com>
2016-02-24 15:33:34 +0200

    Redux using Auth0 from https://auth0.com/blog/2016/01/04/secure-your-react-and-redux-app-with-jwt-authentication/

---
+++ src/js/actions.js
@@ -0,0 +1,174 @@
+
+// The middleware to call the API for quotes
+import { CALL_API } from './middleware/api'
+
+// There are three possible states for our login
+// process and we need actions for each of them
+export const LOGIN_REQUEST = 'LOGIN_REQUEST'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAILURE = 'LOGIN_FAILURE'
+
+function requestLogin(creds) {
+  return {
+    type: LOGIN_REQUEST,
+    isFetching: true,
+    isAuthenticated: false,
+    creds
+  }
+}
+
+function receiveLogin(user) {
+  return {
+    type: LOGIN_SUCCESS,
+    isFetching: false,
+    isAuthenticated: true,
+    id_token: user.id_token
+  }
+}
+
+function loginError(message) {
+  return {
+    type: LOGIN_FAILURE,
+    isFetching: false,
+    isAuthenticated: false,
+    message
+  }
+}
+
+
+// Calls the API to get a token and
+// dispatches actions along the way
+export function loginUser(creds) {
+
+  let config = {
+    method: 'POST',
+    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+    body: `username=${creds.username}&password=${creds.password}`
+  }
+
+  return dispatch => {
+    // We dispatch requestLogin to kickoff the call to the API
+    dispatch(requestLogin(creds))
+
+    return fetch('http://localhost:3001/sessions/create', config)
+      .then(response =>
+        response.json().then(user => ({ user, response }))
+            ).then(({ user, response }) =>  {
+        if (!response.ok) {
+          // If there was a problem, we want to
+          // dispatch the error condition
+          dispatch(loginError(user.message))
+          return Promise.reject(user)
+        } else {
+          // If login was successful, set the token in local storage
+          localStorage.setItem('id_token', user.id_token)
+          // Dispatch the success action
+          dispatch(receiveLogin(user))
+        }
+      }).catch(err => console.log("Error: ", err))
+  }
+}
+
+
+// Three possible states for our logout process as well.
+// Since we are using JWTs, we just need to remove the token
+// from localStorage. These actions are more useful if we
+// were calling the API to log the user out
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE'
+
+function requestLogout() {
+  return {
+    type: LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true
+  }
+}
+
+function receiveLogout() {
+  return {
+    type: LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false
+  }
+}
+
+// Logs the user out
+export function logoutUser() {
+  return dispatch => {
+    dispatch(requestLogout())
+    localStorage.removeItem('id_token')
+    dispatch(receiveLogout())
+  }
+}
+
+
+export const QUOTE_REQUEST = 'QUOTE_REQUEST'
+export const QUOTE_SUCCESS = 'QUOTE_SUCCESS'
+export const QUOTE_FAILURE = 'QUOTE_FAILURE'
+
+// Uses the API middlware to get a quote
+export function fetchQuote() {
+  return {
+    [CALL_API]: {
+      endpoint: 'random-quote',
+      types: [QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE]
+    }
+  }
+}
+
+// Same API middlware is used to get a
+// secret quote, but we set authenticated
+// to true so that the auth header is sent
+export function fetchSecretQuote() {
+  return {
+    [CALL_API]: {
+      endpoint: 'protected/random-quote',
+      authenticated: true,
+      types: [QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE]
+    }
+  }
+}
+
+
+export const SHOW_LOCK = 'SHOW_LOCK'
+export const LOCK_SUCCESS = 'LOCK_SUCCESS'
+export const LOCK_ERROR = 'LOCK_ERROR'
+
+function showLock() {
+  return {
+    type: SHOW_LOCK
+  }
+}
+
+function lockSuccess(profile, token) {
+  return {
+    type: LOCK_SUCCESS,
+    profile,
+    token
+  }
+}
+
+function lockError(err) {
+  return {
+    type: LOCK_ERROR,
+    err
+  }
+}
+
+
+export function login() {
+  const lock = new Auth0Lock('YOUR_CLIENT_ID', 'YOUR_CLIENT_DOMAIN');
+  return dispatch => {
+    lock.show((err, profile, token) => {
+      if(err) {
+        dispatch(lockError(err))
+        return
+      }
+      localStorage.setItem('profile', JSON.stringify(profile))
+      localStorage.setItem('id_token', token)
+      dispatch(lockSuccess(profile, token))
+    })
+  }
+}
---
+++ src/js/containers/App.js
@@ -0,0 +1,64 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { loginUser, fetchQuote, fetchSecretQuote } from '../actions'
+import Login from '../components/Login'
+import Navbar from '../components/Navbar'
+import Quotes from '../components/Quotes'
+
+
+/*
+If we comment out the Quotes component pieces of containers/App.js
+and remove the api middleware call in our index.js file,
+we should be able to log in. There is a default user on the server with
+gonto as the username and password.
+With a successful login, we get the token saved in local storage.
+*/
+class App extends Component {
+  render() {
+    const { dispatch, quote, isAuthenticated, errorMessage, isSecretQuote } = this.props
+    return (
+      <div>
+        <Navbar
+          isAuthenticated={isAuthenticated}
+          errorMessage={errorMessage}
+          dispatch={dispatch}
+        />
+        <div className='container'>
+          <Quotes
+            onQuoteClick={() => dispatch(fetchQuote())}
+            onSecretQuoteClick={() => dispatch(fetchSecretQuote())}
+            isAuthenticated={isAuthenticated}
+            quote={quote}
+            isSecretQuote={isSecretQuote}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  quote: PropTypes.string,
+  isAuthenticated: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string,
+  isSecretQuote: PropTypes.bool.isRequired
+}
+
+// These props come from the application's
+// state when it is started
+function mapStateToProps(state) {
+
+  const { quotes, auth } = state
+  const { quote, authenticated } = quotes
+  const { isAuthenticated, errorMessage } = auth
+
+  return {
+    quote,
+    isSecretQuote: authenticated,
+    isAuthenticated,
+    errorMessage
+  }
+}
+
+export default connect(mapStateToProps)(App)
