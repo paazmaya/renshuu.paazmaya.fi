33542435320a03bd73d84f6f2e50d1afb9382856
Juga Paazmaya <paazmaya@yahoo.com>
2016-02-25 16:34:23 +0200

    Webpack configuration that compiles

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const OUT_PATH = path.join(__dirname, 'dist');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  context: __dirname,
-  entry: [
-    'webpack-hot-middleware/client',
-    'src/js/index.js'
-  ],
+  entry: './src/js/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: OUT_PATH,
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-
-
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-      include: __dirname
-    }, {
-      test: /\.css?$/,
-      loaders: ['style', 'raw'],
-      include: __dirname
-    },
-    {
-      test: /node_modules\/auth0-lock\/.*\.js$/,
-      loaders: [
-        'transform-loader/cacheable?brfs',
-        'transform-loader/cacheable?packageify'
-      ]
-    }, {
-      test: /node_modules\/auth0-lock\/.*\.ejs$/,
-      loader: 'transform-loader/cacheable?ejsify'
-    }, {
-      test: /\.json$/,
-      loader: 'json-loader'
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css?$/,
+        loaders: ['style', 'raw']
+      },
+      {
+        test: /node_modules\/auth0-lock\/.*\.js$/,
+        loaders: [
+          'transform-loader/cacheable?brfs',
+          'transform-loader/cacheable?packageify'
+        ]
+      },
+      {
+        test: /node_modules\/auth0-lock\/.*\.ejs$/,
+        loader: 'transform-loader/cacheable?ejsify'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
   }
 };
 
