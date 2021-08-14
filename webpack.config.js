9aee67f3d8445f852144a3dd78bb9a6360187a61
Juga Paazmaya <paazmaya@yahoo.com>
2021-08-15 00:28:59 +0300

    Keep dependencies almost up to date and just make sure it still builds..

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const OUT_PATH = path.join(__dirname, 'static');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: './src/js/index.js',
+  mode: 'development',
   output: {
     path: OUT_PATH,
     filename: 'bundle.js'
@@ -16,7 +17,7 @@ module.exports = {
     new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         use: ['babel-loader'],
